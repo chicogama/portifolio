85a1412c29009cbba45d0ee2186791cdf53ff2b5
chicogama <josemargama7@gmail.com>
2022-11-23 16:54:05 -0300

    v0.4

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # portifolio
+
 Meu Portifolio
+v.01 Cabeçalho
+v0.2 Rodapé
+v0.3 Página Principal
+v0.4 Página Sobre mim
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
-Particles.init({
+/* Particles.init({
     //options
     selector: '.background',
     maxParticles: 150,
     color: '#153462',
     speed: 0.3,
     connectParticles: true
+}); */
+
+$(document).click(function(e) {
+    $("ul.navbar-nav > li").removeClass("active");
+    $(this).addClass("active");
 });
