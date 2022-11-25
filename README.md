1f77a6b659cdf9ac9465cbd74b13b456020f0965
chicogama <josemargama7@gmail.com>
2022-11-25 00:25:01 -0300

    v0.5

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ v.01 Cabeçalho
 v0.2 Rodapé
 v0.3 Página Principal
 v0.4 Página Sobre mim
+v0.5 Navegando por seções
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
-/* Particles.init({
+Particles.init({
     //options
     selector: '.background',
     maxParticles: 150,
-    color: '#153462',
+    color: '#f9f9f9',
     speed: 0.3,
     connectParticles: true
-}); */
+});
 
-$(document).click(function(e) {
-    $("ul.navbar-nav > li").removeClass("active");
-    $(this).addClass("active");
+$(document).ready(function() {
+    $('ul.navbar-nav > li')
+        .click(function(e) {
+            $('ul.navbar-nav > li')
+                .removeClass('active');
+            $(this).addClass('active');
+        });
 });
