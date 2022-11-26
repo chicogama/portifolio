d614012668458a89d310302f8fe923394809b0b7
chicogama <josemargama7@gmail.com>
2022-11-25 23:44:06 -0300

    v0.5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-Particles.init({
+/* Particles.init({
     //options
     selector: '.background',
     maxParticles: 150,
     color: '#f9f9f9',
     speed: 0.3,
     connectParticles: true
-});
+}); */
 
 $(document).ready(function() {
     $('ul.navbar-nav > li')
