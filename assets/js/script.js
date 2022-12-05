36a10ed60343bfbc72e407173bd44064e962a7d8
chicogama <josemargama7@gmail.com>
2022-12-05 00:35:12 -0300

    v0.7

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,43 @@
-/* Particles.init({
-    //options
-    selector: '.background',
-    maxParticles: 150,
-    color: '#f9f9f9',
-    speed: 0.3,
-    connectParticles: true
-}); */
-
-$(document).ready(function() {
+/* $(document).ready(function() {
     $('ul.navbar-nav > li')
         .click(function(e) {
             $('ul.navbar-nav > li')
                 .removeClass('active');
             $(this).addClass('active');
         });
-});
+}); */
+
+
+class Mensagem {
+
+    constructor() {
+
+    }
+
+    geraId() {
+        return Math.floor(Math.random() * 1000)
+    }
+
+    gravarMsg() {
+        this.mensagens = [];
+        this.formname = document.getElementById('nomeform').value;
+        this.formmail = document.getElementById('mailform').value;
+        this.formmsg = document.getElementById('msgform').value;
+
+        this.novaMensagem({ id: this.geraId(), nome: this.formname, email: this.formmail, msg: this.formmsg })
+        this.nova = '';
+    }
+
+    novaMensagem(msg, nova = true) {
+
+        this.mensagens.push(msg);
+
+        console.log(this.mensagens)
+        if (nova) {
+            localStorage.setItem('mensagem', JSON.stringify(this.mensagens));
+        }
+
+    }
+}
+
+var mensagem = new Mensagem();
