7af4be65878a219b22c9513d208cb98194e43bd1
chicogama <josemargama7@gmail.com>
2022-12-12 00:46:29 -0300

    v0.8

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,12 @@
-/* $(document).ready(function() {
-    $('ul.navbar-nav > li')
-        .click(function(e) {
-            $('ul.navbar-nav > li')
-                .removeClass('active');
-            $(this).addClass('active');
-        });
-}); */
-
-
 class Mensagem {
 
     constructor() {
+        if (JSON.parse(localStorage.getItem('mensagem'))) {
+            this.mensagens = JSON.parse(localStorage.getItem('mensagem'));
+        } else {
 
+            this.mensagens = [];
+        }
     }
 
     geraId() {
@@ -19,25 +14,73 @@ class Mensagem {
     }
 
     gravarMsg() {
-        this.mensagens = [];
         this.formname = document.getElementById('nomeform').value;
         this.formmail = document.getElementById('mailform').value;
         this.formmsg = document.getElementById('msgform').value;
 
         this.novaMensagem({ id: this.geraId(), nome: this.formname, email: this.formmail, msg: this.formmsg })
-        this.nova = '';
+
+        swal("Mensagem enviada", "Sua mensagem foi enviada", "success");
+
+        document.getElementById('nomeform').value = "";
+        document.getElementById('mailform').value = "";
+        document.getElementById('msgform').value = "";
+
     }
 
-    novaMensagem(msg, nova = true) {
-
+    novaMensagem(msg) {
         this.mensagens.push(msg);
-
         console.log(this.mensagens)
-        if (nova) {
-            localStorage.setItem('mensagem', JSON.stringify(this.mensagens));
+        localStorage.setItem('mensagem', JSON.stringify(this.mensagens));
+    }
+
+    listarMsg() {
+        this.msgRecebida = JSON.parse(localStorage.getItem('mensagem'));
+        for (let i = 0; i < this.msgRecebida.length; i++) {
+            let tr = tbody.insertRow();
+
+            let td_name = tr.insertCell();
+            let td_mail = tr.insertCell();
+            let td_msg = tr.insertCell();
+            let td_acoes = tr.insertCell();
+
+            td_name.innerText = this.msgRecebida[i].nome;
+            td_mail.innerText = this.msgRecebida[i].email;
+            td_msg.innerText = this.msgRecebida[i].msg;
+
+            let deleteIcon = document.createElement('i');
+            td_acoes.appendChild(deleteIcon);
+            deleteIcon.classList.add("fa-regular")
+            deleteIcon.classList.add("fa-trash-can");
+            /* deleteIcon.setAttribute("onclick", "mensagem.deletarUma(" + this.arr[i].id + ")"); */
+
         }
+    }
+
+
+    limparMsg() {
+        swal({
+                title: "DELETANDO MENSAGENS",
+                text: "Tem certeza que deseja deletar todas as mensagens?",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+            })
+            .then((willDelete) => {
+                if (willDelete) {
+                    localStorage.clear();
+                    /* alert("Mensagens excluídas"); */
+                    window.location.reload();
+                    swal("Mensagens deletadas", {
+                        icon: "success",
+                    });
+                } else {
+                    swal("Cancelado");
+                }
+            })
 
     }
 }
 
 var mensagem = new Mensagem();
+/* var navbar = new Navbar(); */
